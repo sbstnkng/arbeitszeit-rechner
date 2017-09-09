92bc47ce99f4448d21e81052ec109445074b390d
s3koenig <>
2017-09-09 16:34:12 +0200

    Update script + fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './App';
 
 it('renders without crashing', () => {
+  injectTapEventPlugin();
+
+  const AppContainer = () =>
+    <MuiThemeProvider>
+      <App />
+    </MuiThemeProvider>;
+
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<AppContainer />, div);
 });
