506f08ecf45d05b70586f080d2ff9c9a1ecc530f
s3koenig <>
2017-09-10 02:00:24 +0200

    Add basic handler for arrival time

--- src/components/ResultContainer.js
+++ src/components/ResultContainer.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import ContentCard from './ContentCard';
+import moment from 'moment';
 
 class ResultContainer extends Component {
-  render() {
+  render(props) {
+    function formatDate(date) {
+      if (date !== undefined) {
+        return moment(date).format('hh:mm');
+      }
+    }
+
     return (
       <div>
         <ContentCard title="Ergebnis">
           7,6h erreicht um 16:30 Uhr <br />
           10h erreicht um 19:00 Uhr <br />
           Aktuelle Arbeitszeit: 8,5h (1,1h Überstunden)
+          <br />
+          <br />
+          arrival: {formatDate(this.props.time.arrival)}
         </ContentCard>
       </div>
     );
--- src/components/WorkTimeContainer.js
+++ src/components/WorkTimeContainer.js
@@ -3,11 +3,25 @@ import ContentCard from './ContentCard';
 import TimePickerField from './TimePickerField';
 
 class WorkTimeContainer extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.handleOnChangeKommen = this.handleOnChangeKommen.bind(this);
+  }
+
+  handleOnChangeKommen(event, date) {
+    const time = { arrival: date };
+    this.props.update(time);
+  }
+
+  render(props) {
     return (
       <div>
         <ContentCard title="Arbeitszeit">
-          <TimePickerField label="Kommen" />
+          <TimePickerField
+            label="Kommen"
+            handleOnChange={this.handleOnChangeKommen}
+          />
           <TimePickerField label="Gehen" />
         </ContentCard>
       </div>
