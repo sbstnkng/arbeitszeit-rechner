2cc3a014b5a4c464680bdf9e70b69f15847dad5a
s3koenig <>
2017-11-11 20:09:28 +0100

    Refactor tests of title component

--- src/components/title/Title.test.tsx
+++ src/components/title/Title.test.tsx
@@ -6,10 +6,20 @@ import AppBar from 'material-ui/AppBar';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('title', () => {
+describe('Title', () => {
+  let title: string;
+
+  beforeEach(() => {
+    title = 'I am an awesome title.';
+  });
+
   it('should render a <AppBar /> component', () => {
-    const title: string = 'Imagine here an awesome title.';
     const wrapper = Enzyme.shallow(<Title title={title} />);
     expect(wrapper.find(AppBar)).toHaveLength(1);
+  });
+
+  it('should send the title to the <AppBar /> component', () => {
+    const wrapper = Enzyme.shallow(<Title title={title} />);
+    expect(wrapper.find(AppBar).props().title).toBe(title);
   });
 });
